e14384f87c7205a5f3454caccaf7ee8b75e850e2
fabiowandall <fabiowandall@edu.univali.br>
2019-07-31 22:02:29 -0300

    MeAcorda com alarme v0.1 Alarme colocado manualmente no codigo (linhas 23 e 24). Verifica hora e minuto a cada 10 segundos e varia intencidade dos leds no for.

--- MeAcorda/MeAcorda.ino
+++ MeAcorda/MeAcorda.ino
@@ -1,46 +1,93 @@
-int redPin   = 9;
-int greenPin = 10;
-int bluePin  = 11;
-String inString = "";    // string to hold input
+//Define RTC SD3231 pins
+#define  GND   A2
+#define  VCC   A3
+#define  SDA   A4
+#define  SCL   A5
+
+//Define LED Type GND pins(red, green, blue)
+#define  REDP   9
+#define  GREENP 10
+#define  BLUEP  6
+
+//Carrega a biblioteca do RTC DS1307
+#include <DS1307.h> 
+
+//Modulo RTC DS1307*** ligado as portas A4 e A5 do Arduino UNO(This library work too)
+DS1307 rtc(SDA, SCL);
+
+Time t;
+int maxLight = 50;
+int minLight = 255;
+
+//Set Alarm variables
+int aHour = 23;
+int aMin = 01;
 
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+  while (!Serial){;}
+  Serial.println("\n\nDespertar {%d}:{%d}");
 
-  // send an intro:
-  Serial.println("\n\nString toInt():");
-  Serial.println();
-  
-  // put your setup code here, to run once:
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);  
+  //RTC pins
+  pinMode(GND, OUTPUT);
+  pinMode(VCC, OUTPUT);
+  digitalWrite(GND, LOW);
+  analogWrite(VCC, 170);//~=3,33v
+  //Aciona o relogio
+  rtc.halt(false);
+
+  //LED Type pins
+  pinMode(REDP, OUTPUT);
+  pinMode(GREENP, OUTPUT);
+  pinMode(BLUEP, OUTPUT);  
+
+  turn_off_lights();
+}
+
+void turn_off_lights(){
+  digitalWrite(REDP, HIGH);
+  digitalWrite(GREENP, HIGH);
+  digitalWrite(BLUEP, HIGH);
 }
 
 void loop() {
-  // Read serial input:
-  while (Serial.available() > 0) {
-    int inChar = Serial.read();
-    if (isDigit(inChar)) {
-      // convert the incoming byte to a char and add it to the string:
-      inString += (char)inChar;
-    }
-    // if you get a newline, print the string, then the string's value:
-    if (inChar == '\n') {
-      Serial.print("Value:");
-      Serial.println(inString.toInt());
-      Serial.print("String: ");
-      Serial.println(inString);
+  Serial.println(rtc.getTimeStr());
+  t = rtc.getTime();
 
-      analogWrite(redPin, inString.toInt());
-      analogWrite(greenPin, inString.toInt());
-      analogWrite(bluePin, inString.toInt());
-      
-      // clear the string for new input:
-      inString = "";
+  if(t.hour == aHour){
+    if(t.min == aMin){
+      Serial.println("Alarme");
+      for(int x = minLight; x > maxLight; x--){//Up the light
+        analogWrite(REDP, x);
+        delay(2);
+      }
+      delay(100);
+      for(int x = maxLight; x < minLight; x++){//Down the light
+        analogWrite(REDP, x);
+        delay(2);
+      }
+      for(int x = minLight; x > maxLight; x--){//Up the light
+        analogWrite(GREENP, x);
+        delay(2);
+      }
+      delay(100);
+      for(int x = maxLight; x < minLight; x++){//Down the light
+        analogWrite(GREENP, x);
+        delay(2);
+      }
+      for(int x = minLight; x > maxLight; x--){//Up the light
+        analogWrite(BLUEP, x);
+        delay(2);
+      }
+      delay(100);
+      for(int x = maxLight; x < minLight; x++){//Down the light
+        analogWrite(BLUEP, x);
+        delay(2);
+      }
     }
+    turn_off_lights();   
   }
+  //Aguarda x segundo e repete o processo
+  delay (10000);
 }
